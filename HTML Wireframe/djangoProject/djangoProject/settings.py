86533e57d1a4a9890e1c08617d0793b4b8fbfd0f
Askeladdxx <joshuadellon.jye@gmail.com>
2022-02-02 20:30:57 +0800

    Created a Database

--- HTML Wireframe/djangoProject/djangoProject/settings.py
+++ HTML Wireframe/djangoProject/djangoProject/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',     }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'offenders',     
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost'
+        }
  }
 
 
--- HTML Wireframe/djangoProject/offenderLocatorApp/models.py
+++ HTML Wireframe/djangoProject/offenderLocatorApp/models.py
@@ -1,3 +1,20 @@
+from tabnanny import verbose
 from django.db import models
+from datetime import datetime
+
+from django.forms import CharField, DateField, IntegerField
+
 
 # Create your models here.
+class offenders(models.Model):
+    offender = models.CharField(max_length=45, verbose_name='offender')
+    gender = models.CharField(max_length=6, verbose_name='gender')
+    age = models.PositiveSmallIntegerField(verbose_name='age')
+    offenderID = CharField(verbose_name= 'offenderID')
+    offense = models.CharField(max_length=4, verbose_name='offense')
+    caseStatus= models.CharField(max_length=8, verbose_name='caseStatus')
+    caseDescription = models.TextField(verbose_name='caseDescription')
+    password = models.CharField(max_length=12, verbose_name='password')
+    dateRelease = models.DateField()
+    dateTransfer = models.DateField()
+    dateComplaint = models.DateField()
